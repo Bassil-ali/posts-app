f1bcee333dc295542b3298b7d7f10235643f784d
bassil ali <baselali337@gmail.com>
2020-07-13 09:06:26 -0700

    new laravel project

--- resources/views/include/header1.blade.php
+++ resources/views/include/header1.blade.php
@@ -35,7 +35,7 @@
 
  
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light bg-info">
   <a class="navbar-brand" href="#">{{$blog_name}}</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -67,7 +67,7 @@
     <form class="form-inline my-2 my-lg-0" method="GET" action="/results">
     	{{ csrf_field()}}
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
