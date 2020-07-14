7b8917e1a90a9582b032af2d4bda0e0601b812eb
bassil ali <baselali337@gmail.com>
2020-07-14 05:23:30 -0700

    new laravel project

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -2,7 +2,7 @@
 
 		<!-- PAGE HEADER -->
 		<div class="page-header">
-			<div class="page-header-bg" style="background-image: url('{{$post->featrued}}');"></div>
+			<div class="page-header-bg" style="background-image: secure_url('{{$post->featrued}}');"></div>
 			<div class="container">
 				<div class="row">
 					<div class="col-md-offset-1 col-md-10 text-center">
